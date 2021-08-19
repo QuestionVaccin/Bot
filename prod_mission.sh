35b754e0e87e64930e890d84baae786de40683e2
Erwan Bernard <erwan.bernard@epitech.eu>
2021-08-19 22:06:58 +0200

    prod script

---
+++ prod_mission.sh
@@ -0,0 +1,2 @@
+scp -P 2244 -r ./src/*.py       www.e-bernard.me:/volume1/git/cron/nas-cron/scripts/vaccin-info
+scp -P 2244 -r ./src/utils/*.py www.e-bernard.me:/volume1/git/cron/nas-cron/scripts/vaccin-info/utils
